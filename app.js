4b9a42ee3c1663901818e026373db020bde8ba72
Darthford92 <Rodrigo_v92@outlook.com>
2025-03-16 12:28:09 -0300

    Preparando el app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombreAmigos = [];
+
+function agregarAmigos(amigo) {
+
+    nombreAmigos.push(amigo);
+    
+}
+
+// Funcion que busca un amigo al azar seleccionandolo un número del indice en el arreglo Let nombreAmigos, también tirá un alert si no hay amigos agregados y sale de la funcion
+function amigoAlAzar(){
+    if (nombreAmigos === 0) {
+        alert("¡Aún no has agregado ningún amigo!");
+        return;
+    }
+    Math.floor(Math.random() * nombreAmigos.length);
+    return nombreAmigos[indice];
+}
