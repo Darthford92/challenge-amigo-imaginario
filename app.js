91e6292c28a560ad2f7bd4e2634de83ecd2f363b
Darthford92 <Rodrigo_v92@outlook.com>
2025-03-16 13:27:34 -0300

    Código terminado

--- app.js
+++ app.js
@@ -3,17 +3,37 @@
 let nombreAmigos = [];
 
 function agregarAmigos(amigo) {
+    let input = document.getElementById("amigo");
+    let nombre = input.value;
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+    nombreAmigos.push(nombre);
+    limpiarCaja();
+    mostrarEnPantalla();
+}
 
-    nombreAmigos.push(amigo);
-    
+function limpiarCaja() {
+    let valorCaja = document.querySelector("#amigo");
+    valorCaja.value = "";
 }
 
 // Funcion que busca un amigo al azar seleccionandolo un número del indice en el arreglo Let nombreAmigos, también tirá un alert si no hay amigos agregados y sale de la funcion
-function amigoAlAzar(){
-    if (nombreAmigos === 0) {
+function amigoAlAzar() {
+    if (nombreAmigos.length === 0) {
         alert("¡Aún no has agregado ningún amigo!");
         return;
     }
-    Math.floor(Math.random() * nombreAmigos.length);
-    return nombreAmigos[indice];
+    let indice = Math.floor(Math.random() * nombreAmigos.length);
+    alert("El amigo elegido es: " + nombreAmigos[indice]);
+}
+
+function mostrarEnPantalla(){
+    let lista = document.getElementById("listaAmigos");
+
+    let li = document.createElement("li");
+    li.textContent = nombreAmigos[nombreAmigos.length - 1];
+
+    lista.appendChild(li);
 }
